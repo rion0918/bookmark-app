936517f53997e3a93590d0658f161466d8f9ea7d
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-04-19 21:05:03 +0900

    fix: move src, test, and prisma back into service directory

--- prisma/migrations/20250419084936_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Bookmark" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-
-    CONSTRAINT "Bookmark_pkey" PRIMARY KEY ("id")
-);
--- src/bookmark/bookmark.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BookmarkService } from './bookmark.service';
-import { BookmarkResolver } from './bookmark.resolver';
-
-@Module({
-  providers: [BookmarkService, BookmarkResolver],
-})
-export class BookmarkModule {}
